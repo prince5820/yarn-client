a132ee1185efcd9193ce2e2a5c359575b85c4437
prince-javiya <princejaviya1234@gmail.com>
2024-10-21 17:39:55 +0530

    1. try to fix reload error

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build';
-  return {
-    plugins: [react(), svgr()],
-    base: isProduction ? '/assets/' : '/'
-  }
-});
+export default defineConfig({
+  plugins: [react(), svgr()],
+})
