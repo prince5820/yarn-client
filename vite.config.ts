3c3c2785dc1748ca6333ed8255b0dc28a28c958c
prince-javiya <princejaviya1234@gmail.com>
2024-10-21 17:18:03 +0530

    1. fix when reload page and it's throw page not found

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-})
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  return {
+    plugins: [react(), svgr()],
+    base: isProduction ? '/assets/' : '/'
+  }
+});
